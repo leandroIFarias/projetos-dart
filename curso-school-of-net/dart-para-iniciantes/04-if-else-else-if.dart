8f4a07138cabf90d3cd22ff25060ab8728c65719
leandrorj <leandro__brasil@hotmail.com>
2020-08-05 21:25:45 -0300

    exemplo de if

---
+++ curso-school-of-net/dart-para-iniciantes/04-if-else-else-if.dart
@@ -0,0 +1,14 @@
+void main() {
+
+  var nome = 'Empresa';
+
+  if (nome == 'Empresa'){
+    print('executou o IF');
+  } else if (nome == 'Brasil'){
+    print('Não executou o IF');
+  }
+  else{
+    print('Não foi encontrado nenhuma opcao.');
+  }
+  
+}
