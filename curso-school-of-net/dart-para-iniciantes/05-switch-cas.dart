b7873e932bb8ede636b9b3bcb062b632e57821fc
leandrorj <leandro__brasil@hotmail.com>
2020-08-06 21:49:26 -0300

    exemplo switch em dart

---
+++ curso-school-of-net/dart-para-iniciantes/05-switch-cas.dart
@@ -0,0 +1,15 @@
+void main() {
+  String acao = 'fim';
+
+  switch(acao){
+    case 'inicio':
+      print('iniciando');
+      break; 
+    case 'fim':
+      print('finalizando');
+      break;
+    default:
+      print('...............');
+  }
+   
+}
