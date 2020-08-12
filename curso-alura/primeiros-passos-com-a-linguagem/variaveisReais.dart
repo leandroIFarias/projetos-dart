6435c7a36f05fe7bc61ce106f268543c81c0b81e
leandrorj <leandro__brasil@hotmail.com>
2020-08-12 20:40:47 -0300

    exemplo de double

---
+++ curso-alura/primeiros-passos-com-a-linguagem/variaveisReais.dart
@@ -0,0 +1,8 @@
+void main() {
+  double salario = 3.700;
+  double metadeSalario = salario / 2;
+  double totalAjustado = salario + metadeSalario;
+
+  print("O salario de José é: $salario");
+  print("O ajuste de salario de José sera: $totalAjustado");
+}
