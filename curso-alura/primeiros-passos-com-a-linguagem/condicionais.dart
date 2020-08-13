6bea699cd518c94fd55f30054eba6ba71ab48d3c
leandrorj <leandro__brasil@hotmail.com>
2020-08-12 21:21:42 -0300

    exemplo de condicionais IF ELSE

---
+++ curso-alura/primeiros-passos-com-a-linguagem/condicionais.dart
@@ -0,0 +1,16 @@
+void main() {
+  print("Exemplo de condicionais - IF ELSE");
+  int idade = 18;
+
+  // < significa menor que
+  // > significa maior que
+  // == significa igual a
+  // <= significa menor igual
+  // >= significa menor igual
+
+  if (idade >= 18) {
+    print("Você é maior de idade.");
+  } else {
+    print("Você não é maior de idade.");
+  }
+}
