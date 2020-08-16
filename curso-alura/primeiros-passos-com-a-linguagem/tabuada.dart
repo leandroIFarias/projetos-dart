683ef09d3025b0ce9e3b2c5165f37547d43ed7a0
leandro inacio <leandro.inacio.farias@gmail.com>
2020-08-16 08:53:26 -0300

    exemplo de tabuada com FOR

---
+++ curso-alura/primeiros-passos-com-a-linguagem/tabuada.dart
@@ -0,0 +1,8 @@
+void main() {
+  int resultado;
+  //FOR aninhado
+  for (var contador = 1; contador <= 10; contador++) {
+    resultado = 1 * contador;
+    print("1 x $contador = $resultado");
+  }
+}
