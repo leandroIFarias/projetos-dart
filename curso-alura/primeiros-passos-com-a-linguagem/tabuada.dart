662bb7c7e7362027bcd72ebdc1dcc58bd18b582a
leandro inacio <leandro.inacio.farias@gmail.com>
2020-08-16 09:27:37 -0300

    for aninhado

--- curso-alura/primeiros-passos-com-a-linguagem/tabuada.dart
+++ curso-alura/primeiros-passos-com-a-linguagem/tabuada.dart
@@ -1,8 +1,11 @@
 void main() {
   int resultado;
   //FOR aninhado
-  for (var contador = 1; contador <= 10; contador++) {
-    resultado = 1 * contador;
-    print("1 x $contador = $resultado");
+  for (int multiplicando = 0; multiplicando <= 10; multiplicando++) {
+    print("Tabuada de $multiplicando");
+    for (int contador = 1; contador <= 10; contador++) {
+      print("$multiplicando x $contador = ${multiplicando * contador}");
+    }
+    print("----------\n");
   }
 }
