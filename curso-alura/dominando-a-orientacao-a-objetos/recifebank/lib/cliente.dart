31a6b69ba06b91d9c9485e6fa9b08dd47e067d1d
leandrorj <leandro__brasil@hotmail.com>
2020-08-20 20:54:19 -0300

    separacao do codigo em arquivos

---
+++ curso-alura/dominando-a-orientacao-a-objetos/recifebank/lib/cliente.dart
@@ -0,0 +1,6 @@
+
+class Cliente{
+  String nome;
+  String cpf;
+  String profissao;
+}
---
+++ curso-alura/dominando-a-orientacao-a-objetos/recifebank/lib/contaCorrente.dart
@@ -0,0 +1,46 @@
+import 'cliente.dart';
+
+class ContaCorrente {
+  Cliente titular;
+  int agencia = 145;
+  int conta;
+  double saldo = 20.0;
+  double chequeEspecial = -100.00;
+
+  bool verificaSaldo(double valor){
+    if (this.saldo - valor < chequeEspecial) {
+      print("Sem saldo suficiente.");
+      return false;
+    }else{
+      print("Movimentando $valor reais.");
+      return true;
+    }
+  }
+
+  bool transferencia(double valorDeTransferencia, ContaCorrente contaDestino){
+    if (!verificaSaldo(valorDeTransferencia)) {
+      return false;
+    } else {
+      this.saldo -= valorDeTransferencia;
+      contaDestino.deposito(valorDeTransferencia);
+      return true;
+    }
+  }
+
+  bool saque(double valorDoSaque){
+    if (!verificaSaldo(valorDoSaque)) {
+      return false;
+    } else {
+      this.saldo -= valorDoSaque;
+      return true;
+    }
+  }
+
+  double deposito(double valorDoDeposito){
+    this.saldo += valorDoDeposito;
+    return this.saldo;
+  }
+
+
+
+}
