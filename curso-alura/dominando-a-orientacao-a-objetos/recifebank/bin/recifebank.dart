55ecf945c3d663392d1c560e36fb62fc7df77848
leandrorj <leandro__brasil@hotmail.com>
2020-08-19 22:02:01 -0300

    criacao de metodos

--- curso-alura/dominando-a-orientacao-a-objetos/recifebank/bin/recifebank.dart
+++ curso-alura/dominando-a-orientacao-a-objetos/recifebank/bin/recifebank.dart
@@ -6,7 +6,38 @@ void main() {
   
 class ContaCorrente {
   String titular;
-  int agencia;
+  int agencia = 145;
   int conta;
-  double saldo;
+  double saldo = 20.0;
+  double chequeEspecial = -100.00;
+
+  bool transferencia(double valorDeTransferencia, ContaCorrente contaDestino){
+    if (this.saldo - valorDeTransferencia < chequeEspecial) {
+      print("Sem saldo suficiente.");
+      return false;
+    } else {
+      this.saldo -= valorDeTransferencia;
+      contaDestino.deposito(valorDeTransferencia);
+      return true;
+    }
+  }
+
+  bool saque(double valorDoSaque){
+    if (this.saldo - valorDoSaque < chequeEspecial) {
+      print("Sem saldo suficiente.");
+      return false;
+    } else {
+      print("Sacando $valorDoSaque reais.");
+      this.saldo -= valorDoSaque;
+      return true;
+    }
+  }
+
+  double deposito(double valorDoDeposito){
+    this.saldo += valorDoDeposito;
+    return this.saldo;
+  }
+
+
+
 }
