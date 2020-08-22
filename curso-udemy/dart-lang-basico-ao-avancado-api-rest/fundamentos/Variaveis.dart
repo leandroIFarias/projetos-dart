2667c580639b88aaadc6c410d838501f1c44bfca
leandrorj <leandro__brasil@hotmail.com>
2020-08-22 20:52:45 -0300

    ajuste e criacao de arquivos dart

---
+++ curso-udemy/dart-lang-basico-ao-avancado-api-rest/fundamentos/Variaveis.dart
@@ -0,0 +1,8 @@
+void main() {
+  print('01.0) Variaveis');
+  //Tipos de comentarios:
+    // comentario em linha: (//)
+    // comentario em bloco: (/* bla bla bla */)
+    // comentario de Doc: (///) 
+  
+}
