59eb13da26edc9b185ef00da4fdf398c1c49efd0
leandrorj <leandro__brasil@hotmail.com>
2020-08-31 21:14:08 -0300

    funcao arrow

---
+++ curso-udemy/dart-lang-basico-ao-avancado-api-rest/fundamentos/06-funcoes/FuncoesArrow.dart
@@ -0,0 +1,16 @@
+///
+/// Funcoes Arrow tem retorno imlicito!
+///
+
+funcaoArrow(){
+  print('06.2)Funcoes Arrow\n');
+
+  String conceito() => 'Funcao Arrow com retorno implicito';
+
+  print(conceito());
+}
+
+
+void main() {
+  funcaoArrow();
+}
